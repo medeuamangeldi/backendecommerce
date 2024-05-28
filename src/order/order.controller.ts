e2d02c02b7ae64ed8a7530c1318998322764f0cd
Medeu Amangeldi <medcod49@gmail.com>
2024-05-28 18:42:20 -0500

    add getMe

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,37 +1,40 @@
 import {
-    Body,
-    Controller,
-    Get,
-    Param,
-    Delete,
-    Post,
-    UseGuards,
-    Req,
-  } from '@nestjs/common';
-  import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
-  import { CreateOrderDto } from './dto/create-order.dto';
-  import { OrderService } from './order.service';
-  import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
-  import { Request } from 'express';
-  
-  @Controller('order')
-  @ApiTags('order')
-  export class OrderController {
-    constructor(private orderService: OrderService) {}
-  
-    @Post()
-    @UseGuards(JwtAuthGuard)
-    @ApiBearerAuth()
-    async creatOrder(@Body() createOrderDto: CreateOrderDto, @Req() req: Request) {
-      let userId = req.user["id"];
-      return await this.orderService.create(createOrderDto, userId);
-    }
+  Body,
+  Controller,
+  Get,
+  Param,
+  Delete,
+  Post,
+  UseGuards,
+  Req,
+} from '@nestjs/common';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { CreateOrderDto } from './dto/create-order.dto';
+import { OrderService } from './order.service';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { Request } from 'express';
 
-    @Get()
-    @UseGuards(JwtAuthGuard)
-    @ApiBearerAuth()
-    async getOrders(@Req() req: Request) {
-      let userId = req.user["id"];
-      return await this.orderService.getOrders(userId);
-    }
+@Controller('order')
+@ApiTags('order')
+export class OrderController {
+  constructor(private orderService: OrderService) {}
+
+  @Post()
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  async creatOrder(
+    @Body() createOrderDto: CreateOrderDto,
+    @Req() req: Request,
+  ) {
+    const userId = req.user['id'];
+    return await this.orderService.create(createOrderDto, userId);
+  }
+
+  @Get()
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  async getOrders(@Req() req: Request) {
+    const userId = req.user['id'];
+    return await this.orderService.getOrders(userId);
+  }
 }

c72c73c01a30179bbb9ef097883cdf13cad67ddf
Medeu Amangeldi <medcod49@gmail.com>
2024-07-14 00:23:12 -0500

    feat: selfpicklimit and filledselfpickdates added

--- src/filled-self-pick-date/filled-self-pick-date.module.ts
+++ src/filled-self-pick-date/filled-self-pick-date.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { FilledSelfPickDateService } from './filled-self-pick-date.service';
 import { FilledSelfPickDateController } from './filled-self-pick-date.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   providers: [FilledSelfPickDateService],
   controllers: [FilledSelfPickDateController],
   exports: [FilledSelfPickDateService],
